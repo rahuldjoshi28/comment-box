235d7bb810f5868bf27c2cd2745e6b4703fa6844
Rahul Joshi <rahuljoshi@Rahuls-MacBook-Pro.local>
2021-07-31 21:26:00 +0530

    Adds ability to delete comment

--- src/index.js
+++ src/index.js
@@ -8,34 +8,59 @@ const COMMENT_STORE = "comments"
 
 function commentForm() {
   const addComment = (e) => {
-    update(COMMENT_STORE, prev => [...prev, val(textInput)])
+    const comment = {
+      id: Symbol(),
+      text: val(textInput)
+    }
+    update(COMMENT_STORE, prev => [...prev, comment])
     textInput.value = ""
     e.preventDefault();
   }
-  const textInput = createElement("textarea", {className: "comment-input", placeholder: "Add your comment here..."})
+  const textInput = createElement("textarea", {className: "comment-input", placeholder: "Add your comment here...", required: true})
   const submit = createElement("button", {children: "Submit", type: "submit", className: "button"})
   const form = createElement("form", {className: "comment-form"}, {[Events.SUBMIT]: addComment})
 
   return append(form, textInput, submit)
 }
 
-function comment(text) {
-  return createElement("p", {className: "comment", innerHTML: text})
+function iconButton(icon, props, events) {
+  const i = createElement("i", {className: `fa fa-${icon}`})
+  const btn = createElement("button", props, events)
+  return append(btn, i)
+}
+
+function commentBox({id, text}, handleDelete) {
+  const commentText = createElement("p", {className: "comment-text", innerHTML: text})
+  const del = iconButton("trash", {className: "delete-button"}, {[Events.CLICK]: () => handleDelete(id)})
+
+  const commentContainer = createElement("div", {className: "comment"})
+
+  return append(commentContainer, commentText, del)
 }
 
 function commentList() {
-  const list = comments => comments.map(commentText => comment(commentText));
+  function handleDelete(commentId) {
+    update(COMMENT_STORE, comments => comments.filter(({id}) => id !== commentId))
+  }
+
+  const list = comments => comments.map(comment => commentBox(comment, handleDelete));
 
   const wrapper = createElement("div", {id: "comments", className: "comments"})
 
-  function rerender(comments) {
+  function render(comments) {
+    // cleanup
     wrapper.innerHTML = ""
-    list(comments).forEach(l => wrapper.appendChild(l))
+
+    if (!comments.length) {
+      return append(wrapper, createElement("p", {children: "No comments yet"}))
+    }
+
+    return list(comments).forEach(l => wrapper.appendChild(l))
   }
 
-  subscribe(COMMENT_STORE, rerender)
+  subscribe(COMMENT_STORE, render)
 
-  return append(wrapper, createElement("p", {children: "No comments yet"}))
+  return render([])
 }
 
 createStore(COMMENT_STORE, [])
